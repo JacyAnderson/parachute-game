901ad991065391162f02c9a099f3eeff22b7aa2b
JacyAnderson <jacyjamesanderson@gmail.com>
2017-03-29 15:14:44 -0600

    add collision detection between player one and rockets

--- js/gamepage.js
+++ js/gamepage.js
@@ -9,31 +9,6 @@ var playerOneRight = 0;
 
 var moveLeft = 5;
 
-// var canvas = $('.canvasBoard'),
-//   ctx = canvas.getContext('2d');
-
-//   function draw() {
-//     var c = document.querySelector('canvasBoard');
-//     console.log(c);
-//     if (c.getContext) {
-//       var ctx = c.getContext('2d');
-
-//       ctx.beginPath();
-//       ctx.arc(75, 75, 50, 0, Math.PI * 2, true);
-//       ctx.moveTo(110, 75);
-//       ctx.arc(75, 75, 35, 0, Math.PI, false);
-//       ctx.moveTo(65, 65);
-//       ctx.arc(60, 65, 5, 0, Math.PI * 2, true);
-//       ctx.moveTo(95, 65);
-//       ctx.arc(90, 65, 5, 0, Math.PI * 2, true);
-//       ctx.stroke();
-//     }  
-//   }
-// draw();
-
-
-
-
 /*
 / GRAVITY
 / Simulates gravity on both players
@@ -44,7 +19,7 @@ $(".players").click(function gravity(){
 
 // Function to move playerOne
 $(document).keypress(function movePlayerOne(e) {
-  console.log(e.keyCode);
+  // console.log(e.keyCode);
   
   // Checks for keys pressed by player one
   switch (e.keyCode) {
@@ -57,7 +32,7 @@ $(document).keypress(function movePlayerOne(e) {
  	    })
       }
       leftPressed();
-      console.log('the "a" key pressed');
+      // console.log('the "a" key pressed');
       collision();
       break;
     }
@@ -70,7 +45,7 @@ $(document).keypress(function movePlayerOne(e) {
  	    })
       }
       rightPressed();
-      console.log('the "d" key pressed');
+      // console.log('the "d" key pressed');
       collision();
       break;
     }
@@ -91,8 +66,8 @@ $(document).keypress(function movePlayerTwo(e) {
  	    })
       }
       leftPressed();
-      console.log('the "left arrow" key pressed');
-      collision();
+      // console.log('the "j" key pressed');
+      var result = collision();
       break;
     }
 
@@ -104,7 +79,7 @@ $(document).keypress(function movePlayerTwo(e) {
  	    })
       }
       rightPressed();
-      console.log('the "right arrow" key pressed');
+      // console.log('the "l" key pressed');
       collision();
       break;
     }
@@ -117,8 +92,7 @@ var rocketNum = 1;
 // Creates rockets and launches them across the screen
 function rocketsLeft(){
   var randomTop = Math.floor((Math.random() * 500) + 65);
-  console.log(randomTop);
-  $('<div>Rocket</div>').appendTo(".gameboard").attr('id', 'rocketLeft-' + rocketNum).addClass('rocketLeft');
+  $('<div>Rocket</div>').appendTo(".gameboard").attr('id', 'rocketLeft-' + rocketNum).addClass('rocketLeft rocket');
   $("#rocketLeft-" + rocketNum).css({top: randomTop});
   $("#rocketLeft-" + rocketNum).animate({left: '1300px'}, rocketSpeed);
   rocketNum++
@@ -126,8 +100,7 @@ function rocketsLeft(){
 
 function rocketsRight(){
   var randomTop = Math.floor((Math.random() * 500) + 65);
-  console.log(randomTop);
-  $('<div>Rocket</div>').appendTo(".gameboard").attr('id', 'rocketRight-' + rocketNum).addClass('rocketRight');
+  $('<div>Rocket</div>').appendTo(".gameboard").attr('id', 'rocketRight-' + rocketNum).addClass('rocketRight rocket');
   $("#rocketRight-" + rocketNum).css({top: randomTop});
   $("#rocketRight-" + rocketNum).animate({right: '1300px'}, rocketSpeed);
   rocketNum++
@@ -137,28 +110,133 @@ $('.players').click(function fireRocket() {
   window.setInterval(function() {
     rocketsLeft();
     rocketsRight();
+    rocketPosition();
   }, 3000);
 });
+// time should be 3000
+
 
 /*
-/ Write function that checks for collision between divs
-/
+/ get & store rockets position in sensible way 
+/ delete rockets once off screen 
+/ check collision detection for rockets on screen
+
+console log this to see if it's firing what we want
 */
+function rocketPosition() {
+  $('.rocket').each( function() {
+    var x1 = $('.playerOne').position().left;
+    var y1 = $('.playerOne').position().top;
+    var x2 = $('.playerTwo').position().left;
+    var y2 = $('.playerTwo').position().top;
+    var x3 = $(this).position().left;
+    var y3 = $(this).position().top;
+    // console.log("hello");
+    // console.log(this);
+    if ((y1 + $('.playerOne').outerHeight(true)) < y3 ||
+      y1 > (y3 + $('.playerTwo').outerHeight(true))  ||
+      (x1 + $('.playerOne').outerWidth(true)) < x3 ||
+      x1 > (x3 + $('.playerTwo').outerWidth(true))) {
+        // console.log(false);
+    }else {
+      console.log(true);
+      alert('BOOM!');
+    }
+  });
+}  
+
+setInterval(function() {
+  rocketPosition();
+}, 400);
+    // var rocketPos = $(this).position();
+    // console.log(rocketPos);
+
+
+    
+    // console.log(this + " is hopefully the rocket");
+    // console.log(x3 + " this should be the rocket's left");
+    // console.log(y3 + " this should be the rocket's top");
+    // if ((y1 + $('.playerOne').outerHeight(true)) < y3 ||
+    //   y1 > (y3 + $(this).outerHeight(true))  ||
+    //   (x1 + $('.playerOne').outerWidth(true)) < x3 ||
+    //   x1 > (x3 + $(this).outerWidth(true)) && 
+    //   (y2 + $('.playerTwo').outerHeight(true)) < y3 ||
+    //   y2 > (y3 + $(this).outerHeight(true))  ||
+    //   (x2 + $('.playerTwo').outerWidth(true)) < x3 ||
+    //   x2 > (x3 + $(this).outerWidth(true))) {
+    //   // return false;
+    //   console.log(false + "player has not been hit")
+    // } else { 
+    //   // return true;
+    //   console.log(true + "player has been hit");
+    //   alert('player has been hit');
+    // }
+
+    // if ($('.playerOne').position().right < $(this).position().left ||
+    //     $('.playerOne').position().left > $(this).position().right ||
+    //     $('.playerOne').position().bottom < $(this).position().top||
+    //     $('.playerOne').position().top > $(this).position().bottom) {
+    //     alert("player has been hit");
+    // }
+    // console.log(this);
+  // var x1 = $('.playerOne').position().left;
+  // var y1 = $('.playerOne').position().top;
+  // var x2 = $('.playerTwo').position().left;
+  // var y2 = $('.playerTwo').position().top;
+  // var x3 = $(this).position().left;
+  // var y3 = $(this).position().top;
+
+  // if ((y1 + $('.playerOne').outerHeight(true)) < y3 ||
+  //     y1 > (y3 + $(this).outerHeight(true))  ||
+  //     (x1 + $('.playerOne').outerWidth(true)) < x3 ||
+  //     x1 > (x3 + $(this).outerWidth(true))) {
+  //       // console.log(false);
+  //       alert("collision");
+  //     } else{
+  //       alert("collision");
+  //     }
+ 
+
+// Write function that checks for collision between divs
 function collision(playerOne, playerTwo) {
   var x1 = $('.playerOne').offset().left;
   var y1 = $('.playerOne').offset().top;
   var x2 = $('.playerTwo').offset().left;
   var y2 = $('.playerTwo').offset().top;
-  console.log(x1);
-  console.log(y1);
-  console.log(x2);
-  console.log(y2);
+  // console.log(x1 + "is player one's x coordinate");
+  // console.log(y1 + "is player one's y coordinate");
+  // console.log(x2 + "is player two's x coordinate");
+  // console.log(y2 + "is player two's y coordinate");
   if ((y1 + $('.playerOne').outerHeight(true)) < y2 ||
     y1 > (y2 + $('.playerTwo').outerHeight(true))  ||
     (x1 + $('.playerOne').outerWidth(true)) < x2 ||
-    x1 > (x2 + $('.playerTwo').outerWidth(true))) 
-    console.log(false);
-  else {
-    console.log(true);
+    x1 > (x2 + $('.playerTwo').outerWidth(true))) {
+    // console.log(false);
+  }else {
+    // console.log(true);
+    $('.playerOne').css({
+        left: $('.playerOne').position().left - 30 + "px"
+      })
+    $('.playerTwo').css({
+        right: $('.playerTwo').position().right - 30 + "px"
+      })
   }
 }
+//calculate x and y position for rockets and compare to both players (each player individually)
+
+// function collisionRocket {
+// var x1 = $('.playerOne').offset().left;
+// var y1 = $('.playerOne').offset().top;
+// var x2 = $('.playerTwo').offset().left;
+// var y2 = $('.playerTwo').offset().top;
+// var x3 = $('.rocket').offset().left;
+// var y3 = $('.rocket').offset().top;
+
+//   if ((y1 + $('.playerOne').outerHeight(true)) < y2 ||
+//     y1 > (y2 + $('.playerTwo').outerHeight(true))  ||
+//     (x1 + $('.playerOne').outerWidth(true)) < x2 ||
+//     x1 > (x2 + $('.playerTwo').outerWidth(true))) 
+//     console.log(false);
+//   else {
+//     console.log(true);
+// }
