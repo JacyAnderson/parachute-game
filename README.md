6155d68f3f3076f686bf62c4c0bb5785d0b0a1a5
JacyAnderson <jacyjamesanderson@gmail.com>
2017-03-28 21:18:18 -0600

    Add simple collision detection function for player divs that returns a boolean

--- README.md
+++ README.md
@@ -29,10 +29,15 @@
 ## March 27th, 2017 
 ####	- Dear diary, today I start my mission to construct my first(ish) game. My goal between now and midday tomorrow is to animate my player divs to recreate "gravity" as the players parachute down, and to create a function that allows players to move side to side while parachuting down. I SHOULD take that time to create a function that shoots random div "rockets" from both sides of the screen, but I need to test that the gravity animation and player controls work together first. This will likely bite me in the ass later: I will report back.
 
+### Today's Successes
+#### Got "players" to float down constantly, and move side to side based on player input. Created "gravity" and player input faster than I thought I would - beat my mid-day tomorrow goal.
+
+
 ### Today's Issues
 #### - Git Hub gave me a commit conflict error, but I figured it out with git pull, git fetch, and git merge.
+ - Players can overlap, and leave div due to absolute positioning. Unsure how to solve.
 
-### Today's Successes
+ ##March 28th, 2018
 
 
 
--- js/gamepage.js
+++ js/gamepage.js
@@ -9,23 +9,51 @@ var playerOneRight = 0;
 
 var moveLeft = 5;
 
-//Simulates gravity on both players
+// var canvas = $('.canvasBoard'),
+//   ctx = canvas.getContext('2d');
+
+//   function draw() {
+//     var c = document.querySelector('canvasBoard');
+//     console.log(c);
+//     if (c.getContext) {
+//       var ctx = c.getContext('2d');
+
+//       ctx.beginPath();
+//       ctx.arc(75, 75, 50, 0, Math.PI * 2, true);
+//       ctx.moveTo(110, 75);
+//       ctx.arc(75, 75, 35, 0, Math.PI, false);
+//       ctx.moveTo(65, 65);
+//       ctx.arc(60, 65, 5, 0, Math.PI * 2, true);
+//       ctx.moveTo(95, 65);
+//       ctx.arc(90, 65, 5, 0, Math.PI * 2, true);
+//       ctx.stroke();
+//     }  
+//   }
+// draw();
+
+
+
+
+/*
+/ GRAVITY
+/ Simulates gravity on both players
+*/
 $(".players").click(function gravity(){
-    $(".players").animate({top: '590px'}, 40000);
+    $(".players").animate({top: '590px'}, 30000);
 }); 
 
-//Function to move playerOne
+// Function to move playerOne
 $(document).keypress(function movePlayerOne(e) {
   console.log(e.keyCode);
   
-  //Checks for keys pressed by player one
+  // Checks for keys pressed by player one
   switch (e.keyCode) {
 
-  	//"a" key
+  	// "a" key
     case 97: {
       function leftPressed(){
  	    $('.playerOne').css({
- 	      left: $('.playerOne').position().left - 10 + "px"
+ 	      left: $('.playerOne').position().left - 30 + "px"
  	    })
       }
       leftPressed();
@@ -33,11 +61,11 @@ $(document).keypress(function movePlayerOne(e) {
       break;
     }
 
-    //"d" key
+    // "d" key
     case 100: {
       function rightPressed(){
  	    $('.playerOne').css({
- 	      left: $('.playerOne').position().left + 10 + "px"
+ 	      left: $('.playerOne').position().left + 30 + "px"
  	    })
       }
       rightPressed();
@@ -47,17 +75,17 @@ $(document).keypress(function movePlayerOne(e) {
   }
 });
 
-//Function to move playerTwo
+// Function to move playerTwo
 $(document).keypress(function movePlayerTwo(e) {
   
-  //Checks for keys pressed by player one
+  //Checks for keys pressed by player two
   switch (e.keyCode) {
 
-  	//"j" key
+  	// "j" key
     case 106: {
       function leftPressed(){
  	    $('.playerTwo').css({
- 	      left: $('.playerTwo').position().left - 10 + "px"
+ 	      left: $('.playerTwo').position().left - 30 + "px"
  	    })
       }
       leftPressed();
@@ -65,11 +93,11 @@ $(document).keypress(function movePlayerTwo(e) {
       break;
     }
 
-    //"l" key
+    // "l" key
     case 108: {
       function rightPressed(){
  	    $('.playerTwo').css({
- 	      left: $('.playerTwo').position().left + 10 + "px"
+ 	      left: $('.playerTwo').position().left + 30 + "px"
  	    })
       }
       rightPressed();
@@ -78,3 +106,56 @@ $(document).keypress(function movePlayerTwo(e) {
     }
   }
 });
+
+// Sets initial rocket speed
+var rocketSpeed = 13000;
+var rocketNum = 1;
+// Creates rockets and launches them across the screen
+function rocketsLeft(){
+  var randomTop = Math.floor((Math.random() * 500) + 65);
+  console.log(randomTop);
+  $('<div>Rocket</div>').appendTo(".gameboard").attr('id', 'rocketLeft-' + rocketNum).addClass('rocketLeft');
+  $("#rocketLeft-" + rocketNum).css({top: randomTop});
+  $("#rocketLeft-" + rocketNum).animate({left: '1300px'}, rocketSpeed);
+  rocketNum++
+}
+
+function rocketsRight(){
+  var randomTop = Math.floor((Math.random() * 500) + 65);
+  console.log(randomTop);
+  $('<div>Rocket</div>').appendTo(".gameboard").attr('id', 'rocketRight-' + rocketNum).addClass('rocketRight');
+  $("#rocketRight-" + rocketNum).css({top: randomTop});
+  $("#rocketRight-" + rocketNum).animate({right: '1300px'}, rocketSpeed);
+  rocketNum++
+}
+
+$('.players').click(function fireRocket() {
+  window.setInterval(function() {
+    rocketsLeft();
+    rocketsRight();
+  }, 3000);
+});
+
+/*
+/ Write function that checks for collision between divs
+/
+*/
+function collision(playerOne, playerTwo) {
+  var x1 = $('.playerOne').offset().left;
+  var y1 = $('.playerOne').offset().top;
+  var x2 = $('.playerTwo').offset().left;
+  var y2 = $('.playerTwo').offset().top;
+  console.log(x1);
+  console.log(y1);
+  console.log(x2);
+  console.log(y2);
+  if ((y1 + $('.playerOne').outerHeight(true)) < y2 ||
+    y1 > (y2 + $('.playerTwo').outerHeight(true))  ||
+    (x1 + $('.playerOne').outerWidth(true)) < x2 ||
+    x1 > (x2 + $('.playerTwo').outerWidth(true))) 
+    console.log(false);
+  else {
+    console.log(true);
+  }
+}
+collision();
